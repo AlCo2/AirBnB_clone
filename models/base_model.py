9759b1a44bcb1323f6dbffd340060b363e1848dd
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-02-05 22:48:54 +0100

    add doc for all methods

--- models/base_model.py
+++ models/base_model.py
@@ -12,14 +12,24 @@ class BaseModel:
     Base Model class definition
     """
     def __init__(self):
+        """
+        init the class BaseModel
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def save(self):
+        """
+        updates the public instance attr updated_at
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        return a dict containing all keys/values
+        of __dict__
+        """
         newdict = self.__dict__.copy()
         newdict['created_at'] = self.created_at.isoformat()
         newdict['updated_at'] = self.updated_at.isoformat()
@@ -27,6 +37,9 @@ class BaseModel:
         return newdict
 
     def __str__(self):
+        """
+        print [<class name>] (<self.id>) <self.__dict__>
+        """
         reprstr = '[' + str(type(self).__name__) + '] '
         reprstr += '(' + self.id + ') ' + str(self.__dict__)
         return reprstr
