e66405eda2bb2b9424fa22ab9d9de04ecea2035f
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-02-06 12:49:22 +0100

    more test cases

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -13,8 +13,30 @@ class testBaseModel(unittest.TestCase):
         base = BaseModel()
         base.name = "test_name"
         self.assertEqual(base.name, "test_name")
+
+    def test_idType(self):
+        base = BaseModel()
+        self.assertEqual(str, type(base.id))
     
     def test_number(self):
         base = BaseModel()
         base.my_number = 89
         self.assertEqual(base.my_number, 89)
+
+    def test_add(self):
+        base = BaseModel()
+        base.my_number = 1
+        base.my_number += 1
+        self.assertEqual(base.my_number, 2)
+
+    def test_save(self):
+        base = BaseModel()
+        oldtime = base.updated_at
+        base.save()
+        self.assertNotEqual(base.updated_at, oldtime)
+
+    def test_save_created_at(self):
+        base = BaseModel()
+        old_created_at = base.created_at
+        base.save()
+        self.assertEqual(base.created_at, old_created_at)
