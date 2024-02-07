07c3d9e6cfffd364cc6326ded58cd01344801894
rzsamrio <samuelekop144@gmail.com>
2024-02-07 02:27:23 +0100

    Add Task 4: KWAGS

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,23 @@ class BaseModel:
     """
     Base Model class definition
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         init the class BaseModel
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key in kwargs:
+                if key == '__class__':
+                    continue
+                elif key in ["created_at", "updated_at"]:
+                    setattr(self, key, datetime.strptime(kwargs[key],
+                            '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, key, kwargs[key])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """
