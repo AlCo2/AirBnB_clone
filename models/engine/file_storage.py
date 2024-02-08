5e1a02b0b57d1e70fc5a9f39679fb76a5f39a581
rzsamrio <samuelekop144@gmail.com>
2024-02-08 08:41:07 +0100

    Map object dictionary to object rather than obj.to_dict()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,23 +16,27 @@ class FileStorage:
 
     def all(self):
         """ Returns all saved objects in storage """
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """ Records the new object in JSON for storage """
         FileStorage.__objects[type(obj).__name__ + "."
-                              + obj.id] = obj.to_dict()
-        # Should have it assigned to obj. Redo.
+                              + obj.id] = obj
 
     def save(self):
         """ Save the JSON File to storage """
+        tmp = {}
+        for key, val in FileStorage.__objects.items():
+            tmp[key] = val.to_dict()
         with open(FileStorage.__file_path, 'w', encoding='utf-8') as db:
-            json.dump(FileStorage.__objects, db)
+            json.dump(tmp, db)
 
     def reload(self):
         """ Deserialize the json file for information retrieval """
         try:
             with open(FileStorage.__file_path, 'r') as db:
-                FileStorage. __objects = json.load(db)
+                FileStorage.__objects = json.load(db)
+            for key, obj in FileStorage.__objects.items():
+                FileStorage.__objects[key] = eval(obj["__class__"])(**obj)
         except Exception:
             pass
