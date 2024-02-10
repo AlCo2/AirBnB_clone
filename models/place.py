f73efe41c260de5848ca245aff793e769f0fd15d
rzsamrio <samuelekop144@gmail.com>
2024-02-10 18:08:01 +0100

    pycodestyle

--- models/place.py
+++ models/place.py
@@ -4,15 +4,15 @@ from base_model import BaseModel
 
 
 def Place(BaseModel):
-	""" Describes a specific area or location """
-	city_id = ""
-	user_id = ""
-	name = ""
-	description = ""
-	number_rooms = 0
-	number_bathrooms = 0
-	max_guest = 0
-	price_by_night = 0
-	latitude = 0.0
-	longitude = 0.0
-	amenity_ids = []
+    """ Describes a specific area or location """
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
