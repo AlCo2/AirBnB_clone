4ad354c2f9a0a7cce15e769f6564eb93c5a405aa
rzsamrio <samuelekop144@gmail.com>
2024-02-10 18:33:00 +0100

    Remove executable

--- models/place.py
+++ models/place.py
@@ -16,3 +16,7 @@ def Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """ Initialise the superclass if called """
+        super.__init__(*args, **kwargs)
