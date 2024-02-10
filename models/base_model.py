bdad601feea2a6fa5f462901370d09ccc574a754
AlCo2 <abdou.lahboub.wwe@gmail.com>
2024-02-11 00:18:19 +0100

    change updated at

--- models/base_model.py
+++ models/base_model.py
@@ -12,6 +12,7 @@ class BaseModel:
     """
     Base Model class definition
     """
+
     def __init__(self, *args, **kwargs):
         """
         init the class BaseModel
@@ -28,7 +29,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
 
     def save(self):
